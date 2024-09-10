e54561036a537b83f3c022d8d22597140d3ffbda
dndannynguyen <dn.dannynguyen@hotmail.com>
2024-09-09 17:26:32 -0700

    Finish Zodiac Finder

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,15 +5,47 @@ namespace Zodiac.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    [BindProperty]
+    public int Birthyear { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public string ZodiacImageUrl { get; set; } = "/images/questionmark.png";
+
+    public string ZodiacAnimal {get; set;} = "";
+
+    public void OnPost()
     {
-        _logger = logger;
+        ZodiacImageUrl = GetZodiacAnimalImage(Birthyear);
+        ZodiacAnimal = GetZodiacAnimal(Birthyear);
     }
 
-    public void OnGet()
+    private string GetZodiacAnimalImage(int birthyear)
     {
+        // Logic to calculate Zodiac animal based on birthyear
+        // Here we return the corresponding image URL based on the year
+        int zodiacIndex = (birthyear - 1900) % 12;
+        string[] zodiacImages = {
+            "/images/rat.png", "/images/ox.png", "/images/tiger.png",
+            "/images/rabbit.png", "/images/dragon.png", "/images/snake.png",
+            "/images/horse.png", "/images/goat.png", "/images/monkey.png",
+            "/images/rooster.png", "/images/dog.png", "/images/pig.png"
+        };
 
+        return zodiacImages[zodiacIndex];
+    }
+
+    private string GetZodiacAnimal(int birthyear)
+    {
+        // Logic to calculate Zodiac animal based on birthyear
+        // Here we return the corresponding animal based on the year
+        int zodiacIndex = (birthyear - 1900) % 12;
+        string[] zodiac = {
+            "Rat", "Ox", "Tiger",
+            "Rabbit", "Dragon", "Snake",
+            "Horse", "Goat", "Monkey",
+            "Rooster", "Dog", "Pig"
+        };
+
+        return zodiac[zodiacIndex];
     }
 }
+
